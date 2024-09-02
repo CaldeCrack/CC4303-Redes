2570e7a07c9a2514d44e2221d3cb29c6eac3f351
CaldeCrack <andres.calderon@ug.uchile.cl>
2024-09-02 17:27:52 -0400

    Parte 1 lista

--- tareas/tarea_1/client_bw.py
+++ tareas/tarea_1/client_bw.py
@@ -1,37 +1,40 @@
 #!/usr/bin/python3
 # Echo client program
 # Version con dos threads: uno lee de stdin hacia el socket y el otro al revés
-import jsockets
-import sys, threading
-import time
+import jsockets, sys, threading, time
 
-def Rdr(s):
-    while True:
-        try:
-            data=s.recv(1500).decode()
-        except:
-            data = None
-        if not data: 
-            break
-        print(data, end = '')
+def Rdr(s, size):
+	global total_size
+	while True:
+		try:
+			data = s.recv(size)
+			if not data:
+				break
+			sys.stdout.buffer.write(data)
+		except:
+			break
 
-if len(sys.argv) != 3:
-    print('Use: '+sys.argv[0]+' host port')
-    sys.exit(1)
+if len(sys.argv) != 4:
+	print(f"Use: {sys.argv[0]} size host port")
+	sys.exit(1)
 
-s = jsockets.socket_tcp_connect(sys.argv[1], sys.argv[2])
+s = jsockets.socket_tcp_connect(sys.argv[2], sys.argv[3])
 if s is None:
-    print('could not open socket')
-    sys.exit(1)
+	print('Could not open socket')
+	sys.exit(1)
 
 # Creo thread que lee desde el socket hacia stdout:
-newthread = threading.Thread(target=Rdr, args=(s,))
+size: int = int(sys.argv[1])
+newthread = threading.Thread(target=Rdr, args=(s, size))
 newthread.start()
 
 # En este otro thread leo desde stdin hacia socket:
-for line in sys.stdin:
-    s.send(line.encode())
+while True:
+	byte_s = sys.stdin.buffer.read(size)
+	if not byte_s:
+		s.shutdown(jsockets.socket.SHUT_WR)
+		break
+	s.send(byte_s)
 
-time.sleep(3)  # dar tiempo para que vuelva la respuesta
+time.sleep(3)
 s.close()
-
--- tareas/tarea_1/create_files.py
+++ tareas/tarea_1/create_files.py
@@ -2,6 +2,14 @@
 import sys, os
 
 def main(file_size: int, file_amount: int):
+	# Delete files in folder
+	path = os.path.dirname(os.path.abspath(__file__))
+	path += "/files"
+	folder = os.listdir(path)
+	for f in folder:
+		os.remove(f"{path}/{f}")
+
+	# Create files
 	size_bytes = file_size * 1024 * 1024
 	for i in range(file_amount):
 		with open(f"files/archivo_{i}.bin", 'wb') as f:
