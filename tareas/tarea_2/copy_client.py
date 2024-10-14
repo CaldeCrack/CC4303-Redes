13b091c739531535270cce96b53eef7955cc3f9d
CaldeCrack <andres.calderon@ug.uchile.cl>
2024-10-14 17:08:33 -0300

    Algo cerca del selective-repeat creo

--- tareas/tarea_2/copy_client.py
+++ tareas/tarea_2/copy_client.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 # Selective-Repeat simulation
-import jsockets, sys, threading
+import jsockets, sys, threading, queue
+from datetime import datetime, timedelta
 
 
 WIN_SZ_LIMIT = 32767
@@ -14,9 +15,12 @@ win_sz: int = int(sys.argv[2])
 host: str = sys.argv[3]
 port: str = sys.argv[4]
 
-if not (1 <= sys.argv[2] <= WIN_SZ_LIMIT):
+if not (1 <= win_sz <= WIN_SZ_LIMIT):
 	print(f"win_sz should be between 1 and 32767 (given {win_sz})")
 	sys.exit(1)
+
+recv_window: list = [(i, False) for i in range(win_sz)] # sequence number, ack
+sndr_window: list = [(i, False, None) for i in range(win_sz)] # sequence number, ack, data
 
 def sequence_number():
 	num: int = 0
@@ -26,31 +30,32 @@ def sequence_number():
 		yield byte_number
 		num = (num + 1) % limit
 
-def advance_window(window: list) -> int:
-	while window[0][1]:
-		del window[0]
-		window.append(((window[-1][0] + 1) % WIN_SZ_LIMIT, False))
-	#! return pendiente
+def advance_window(win: list, q=None) -> int:
+	while win[0][1]:
+		del win[0]
+		win.append(((win[-1][0] + 1) % WIN_SZ_LIMIT, False))
+		if q:
+			q.get()
+	return win[0][0]
 
-# receptor
+# ########### RECEPTOR ###########
 def Rdr(s, pack_sz, win_sz):
-	win_min: int = 0
-	win_max: int = win_sz - 1
-	window: list = [(i, False) for i in range(win_sz)]
+	recv_min: int = 0
+	recv_max: int = win_sz - 1
 
 	while True:
 		try:
-			data = s.recv(pack_sz)
-			recv_sqn: int = int.from_bytes(data[0:2], "big")
-			index: int = recv_sqn - win_min
+			recv_sqn: int = int.from_bytes(s.recv(2), "big")
+			data: bytes = s.recv(pack_sz - 2)
+			index: int = recv_sqn - recv_min
 
-			if (win_min <= recv_sqn <= win_max):
-				window[index] = (recv_sqn, True)
+			if (recv_min <= recv_sqn <= recv_max):
+				recv_window[index] = (recv_sqn, True)
+				sndr_window[index] = (recv_sqn, True, data)
 
-			if recv_sqn == win_min:
-				win_min = advance_window(window)
-				win_max = win_min + win_sz
-
+			if recv_sqn == recv_min:
+				recv_min = advance_window(recv_window)
+				recv_max = recv_min + win_sz
 
 			sys.stdout.buffer.write(data)
 			if len(data) == 2:
@@ -63,17 +68,53 @@ if s is None:
 	print('Could not open socket')
 	sys.exit(1)
 
-newthread = threading.Thread(target=Rdr, args=(s, pack_sz, win_sz))
-newthread.start()
-sqn = sequence_number()
+receiver = threading.Thread(target=Rdr, args=(s, pack_sz, win_sz))
+receiver.start()
 
-# emisor
+
+# ########### EMISOR ###########
+def peek_time(pq):
+	return pq.queue[0][0]
+
+def peek_value(pq) -> int:
+	return pq.queue[0][1]
+
+timeout: float = 0.5
+sqn = sequence_number()
+sndr_min: int = 0
+sndr_max: int = win_sz - 1
+timeouts = queue.PriorityQueue() # time, sequence number
+n: int = next(sqn)
+
 while True:
 	byte_s = sys.stdin.buffer.read(pack_sz - 2)
-	if not byte_s:
-		s.shutdown(jsockets.socket.SHUT_WR)
-		break
-	s.send(next(sqn) + byte_s)
+	int_n: int = int.from_bytes(n, "big")
 
-newthread.join()
+	if sndr_min <= int_n <= sndr_max and byte_s:
+		s.send(n + byte_s)
+		sndr_window[int_n - sndr_min] = (int_n, False, byte_s)
+		delta = timedelta(seconds=timeout)
+		expire = (datetime.now() + delta, int_n)
+		timeouts.put(expire)
+		n = next(sqn)
+	elif not byte_s:
+		s.send(n)
+
+	if datetime.now() >= peek_time(timeouts):
+		# print(timeouts.qsize())
+		index: int = peek_value(timeouts) - sndr_min
+		if not (packet := sndr_window[index])[1]: # retransmitir paquete
+			timeouts.get()
+			s.send(packet[0].to_bytes(2, "big") + packet[2])
+			delta = timedelta(seconds=timeout)
+			expire = (datetime.now() + delta, int_n)
+			timeouts.put(expire)
+		elif sndr_min == peek_value(timeouts):
+			sndr_min = advance_window(sndr_window, timeouts)
+			sndr_max = sndr_min + win_sz
+
+	if timeouts.empty():
+		break
+
+receiver.join()
 s.close()
